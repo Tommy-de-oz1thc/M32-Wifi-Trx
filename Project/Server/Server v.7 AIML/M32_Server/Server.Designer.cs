3a31b945f4c88f99b60675d27aef28e387fb5f4b
Tommy-de-oz1thc <60524067+Tommy-de-oz1thc@users.noreply.github.com>
2020-07-24 11:11:32 +0200

    Add files via upload

---
+++ Project/Server/Server v.7 AIML/M32_Server/Server.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace M32_Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Server));
+            this.txtSentText = new System.Windows.Forms.TextBox();
+            this.labStatus = new System.Windows.Forms.Label();
+            this.labSentText = new System.Windows.Forms.Label();
+            this.btnSent = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pic_M32 = new System.Windows.Forms.PictureBox();
+            this.labCall = new System.Windows.Forms.Label();
+            this.txtStatusText = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.labVErsion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_M32)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSentText
+            // 
+            this.txtSentText.Location = new System.Drawing.Point(26, 547);
+            this.txtSentText.Name = "txtSentText";
+            this.txtSentText.Size = new System.Drawing.Size(351, 20);
+            this.txtSentText.TabIndex = 5;
+            // 
+            // labStatus
+            // 
+            this.labStatus.AutoSize = true;
+            this.labStatus.Location = new System.Drawing.Point(23, 204);
+            this.labStatus.Name = "labStatus";
+            this.labStatus.Size = new System.Drawing.Size(64, 13);
+            this.labStatus.TabIndex = 6;
+            this.labStatus.Text = "Status Text:";
+            // 
+            // labSentText
+            // 
+            this.labSentText.AutoSize = true;
+            this.labSentText.Location = new System.Drawing.Point(23, 515);
+            this.labSentText.Name = "labSentText";
+            this.labSentText.Size = new System.Drawing.Size(56, 13);
+            this.labSentText.TabIndex = 7;
+            this.labSentText.Text = "Sent Text:";
+            // 
+            // btnSent
+            // 
+            this.btnSent.Location = new System.Drawing.Point(406, 545);
+            this.btnSent.Name = "btnSent";
+            this.btnSent.Size = new System.Drawing.Size(90, 23);
+            this.btnSent.TabIndex = 8;
+            this.btnSent.Text = "Sent to M32";
+            this.btnSent.UseVisualStyleBackColor = true;
+            this.btnSent.Click += new System.EventHandler(this.btnSent_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(396, 131);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(95, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pic_M32
+            // 
+            this.pic_M32.Image = ((System.Drawing.Image)(resources.GetObject("pic_M32.Image")));
+            this.pic_M32.Location = new System.Drawing.Point(26, 29);
+            this.pic_M32.Name = "pic_M32";
+            this.pic_M32.Size = new System.Drawing.Size(330, 166);
+            this.pic_M32.TabIndex = 11;
+            this.pic_M32.TabStop = false;
+            // 
+            // labCall
+            // 
+            this.labCall.AutoSize = true;
+            this.labCall.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labCall.Location = new System.Drawing.Point(377, 29);
+            this.labCall.Name = "labCall";
+            this.labCall.Size = new System.Drawing.Size(138, 66);
+            this.labCall.TabIndex = 12;
+            this.labCall.Text = "OZ1THC\r\nTommy";
+            // 
+            // txtStatusText
+            // 
+            this.txtStatusText.Location = new System.Drawing.Point(26, 235);
+            this.txtStatusText.Multiline = true;
+            this.txtStatusText.Name = "txtStatusText";
+            this.txtStatusText.ReadOnly = true;
+            this.txtStatusText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatusText.Size = new System.Drawing.Size(351, 258);
+            this.txtStatusText.TabIndex = 13;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(396, 172);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(95, 23);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "Stop Server";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // labVErsion
+            // 
+            this.labVErsion.AutoSize = true;
+            this.labVErsion.Location = new System.Drawing.Point(415, 480);
+            this.labVErsion.Name = "labVErsion";
+            this.labVErsion.Size = new System.Drawing.Size(88, 13);
+            this.labVErsion.TabIndex = 14;
+            this.labVErsion.Text = "Version v.7 AIML";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 604);
+            this.Controls.Add(this.labVErsion);
+            this.Controls.Add(this.txtStatusText);
+            this.Controls.Add(this.labCall);
+            this.Controls.Add(this.pic_M32);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSent);
+            this.Controls.Add(this.labSentText);
+            this.Controls.Add(this.labStatus);
+            this.Controls.Add(this.txtSentText);
+            this.Name = "Server";
+            this.Text = "M32 Server ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Server_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_M32)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSentText;
+        private System.Windows.Forms.Label labStatus;
+        private System.Windows.Forms.Label labSentText;
+        private System.Windows.Forms.Button btnSent;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pic_M32;
+        private System.Windows.Forms.Label labCall;
+        private System.Windows.Forms.TextBox txtStatusText;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label labVErsion;
+    }
+}
+
---
+++ Project/Server/Server v.7 AIML/M32_Server/Server.cs
@@ -0,0 +1,565 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using M32_Server.Bots;
+
+
+namespace M32_Server
+{
+    public partial class Server : Form
+    {
+        private int recv;
+        private byte[] data = new byte[1024];
+        private Mopp mopp = new Mopp();
+        private IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 7373);
+
+        private Socket newsock = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Dgram, ProtocolType.Udp);
+        private IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        private EndPoint Remote;
+        private Thread run_Server;
+        private bool isConnected = false;
+        private string m32_call = "oz1thc";
+   
+      
+        string status_text = "";
+        private static Bots.Bots bots;
+        public Server()
+        {
+            InitializeComponent();
+            run_Server = new Thread(new ThreadStart(Start_Server));
+           
+
+        }
+
+       
+
+        private void Start_Server()
+        {
+            
+           
+            AppendTextBox("Waiting for a client...\r\n");
+            try
+            {
+                newsock.Bind(ipep);
+                Remote = (EndPoint)(sender);
+               
+                
+            }
+            catch { }
+            Loop_Start();
+          
+        }
+        
+        private void Loop_Start()
+        {
+            bots = new Bots.Bots();
+            while (true)
+            {
+                data = new byte[1024];
+                string read = "";
+                recv = newsock.ReceiveFrom(data, ref Remote);
+                read = mopp.Read_Mopp_Word(data);
+                
+                if (read == "qrl?")
+                {
+                    AppendTextBox("M32: " + read + "\r\n");
+                    string text = "k";
+                    string[] sent = text.Split(' ');
+                    foreach (var t in sent)
+                    {
+                        data = mopp.Sent_Mopp_Word(t);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                    AppendTextBox("Server: " + text + "\r\n");
+                    
+                    Loop_Bots();
+                    break;
+                }
+                else if (read == "echo?")
+                {
+                    AppendTextBox("M32: " + read + "\r\n");
+                    string text = "k";
+                    string[] sent = text.Split(' ');
+                    foreach (var t in sent)
+                    {
+                        data = mopp.Sent_Mopp_Word(t);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                    AppendTextBox("Server: " + text + "\r\n");
+
+                    Loop_Repeat();
+                    break;
+                }
+                else if (read == "talk?")
+                {
+                    AppendTextBox("M32: " + read + "\r\n");
+                    string text = "k";
+                    string[] sent = text.Split(' ');
+                    foreach (var t in sent)
+                    {
+                        data = mopp.Sent_Mopp_Word(t);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                    AppendTextBox("Server: " + text + "\r\n");
+
+                    Loop_Small_Talk();
+                    break;
+                }
+
+            }
+        }
+        private string Get_CQ_Call()
+        {
+            string read = "";
+            string read_tx = "";
+            string response = "";
+            
+
+            while (true)
+            {
+
+                data = new byte[1024];
+                read = "";
+                recv = newsock.ReceiveFrom(data, ref Remote);
+                read = mopp.Read_Mopp_Word(data);
+                if ((read != "k")&&(read!="<sk>"))
+                {
+                    if (read == "*")
+                    {
+                        read_tx = read_tx.Trim();
+                        for (var a = read_tx.Length - 1; a > 0; a--)
+                        {
+                            if (read_tx[a] != ' ')
+                            {
+                                read_tx =read_tx.Remove(a);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        read_tx += read + " ";
+                    }
+                }
+                    if ((read == "<sk>")|| (read == "k"))
+                {
+                    AppendTextBox("Client: " + read_tx + " "+ read + "\r\n");
+                    if (read_tx.Contains("cq de "))
+                    {
+                        string toBeSearched = "cq de ";
+                        string m32_call_R = read_tx.Substring(read_tx.IndexOf(toBeSearched) + toBeSearched.Length).Trim();
+                       
+                        string[] m32_call_a = m32_call_R.Split(' ');
+                        int call_nr = 0;
+                        m32_call = m32_call_a[0];
+                        foreach(var c in m32_call_a)
+                        {
+                            call_nr = 0;
+                            string ca = c;
+                            foreach(var cb in m32_call_a)
+                            {
+                                if(cb==ca)
+                                { call_nr++; }
+                                if(call_nr>1)
+                                { m32_call = cb; break; }
+                            }
+                        }
+                        response = bots.Talk_Whit_Bot(m32_call, read_tx, "F");
+                        string response_s = response.Replace("<kn>", "$");
+                        string[] response_s1 = response_s.Split(' ');
+                        read_tx = "";
+                        foreach (var s1 in response_s1)
+                        {
+                           
+                            data = mopp.Sent_Mopp_Word(s1);
+                            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                           // string text = mopp.Read_Mopp_Word(data);
+                           // AppendTextBox("sent: " + text + "\r\n");
+                        }
+                        AppendTextBox("Server: "+ response + "\r\n");
+                        QSO(response);
+                    }
+                    if(read=="<sk")
+                    {
+                        response = "bye <sk>";
+                        string response_s = response.Replace("<sk>", "@");
+                        string[] response_s1 = response_s.Split(' ');
+                       
+                        foreach (var s1 in response_s1)
+                        {
+
+                            data = mopp.Sent_Mopp_Word(s1);
+                            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                            
+                        }
+                        AppendTextBox("Server: " + response + "\r\n");
+                        Loop_Start();
+                    }
+                    break;
+                }
+            }
+            return read;
+        }
+
+        private void QSO(string callsign)
+        {
+            string read_tx = "";
+            string response = "";
+            while (true)
+            {
+                data = new byte[1024];
+                recv = newsock.ReceiveFrom(data, ref Remote);
+                string read = mopp.Read_Mopp_Word(data);
+                if ((read != "<kn>") || (read != "<sk>"))
+                {
+
+                    if (read == "*")
+                    {
+                        read_tx = read_tx.Trim();
+                        for (var a = read_tx.Length - 1; a > 0; a--)
+                        {
+                            if (read_tx[a] != ' ')
+                            {
+                                read_tx = read_tx.Remove(a);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        read_tx += read + " ";
+                    }
+                }
+                if(read=="<kn>")
+                {
+                    AppendTextBox("Client: " + read_tx + "\r\n");
+                    response = "";
+                    response = bots.Talk_Whit_Bot(m32_call, read_tx, "M");
+                    response = response.Replace("<kn>", "");
+                    response += " <kn>";
+                    read_tx = "";
+                    string response_r = response.Replace("<kn>", "$");
+                    string[] response_s = response_r.Split(' ');
+                    foreach(var w in response_s)
+                    {
+                        data = mopp.Sent_Mopp_Word(w);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                    
+                    AppendTextBox("Server: " + response + "\r\n");
+                }
+                if(read=="<sk>")
+                {
+                    AppendTextBox("Client: " + read_tx + "\r\n");
+                    response = callsign + "tnx fer nice qso 73 73 " + callsign + "<sk>";
+                    response = response.Replace("<kn>", "");
+                    string response_r = response.Replace("<sk>", "@");
+                    string[] response_s = response_r.Split(' ');
+                    foreach (var w in response_s)
+                    {
+                        data = mopp.Sent_Mopp_Word(w);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                    AppendTextBox("Server: " + response + "\r\n");
+                    Loop_Start();
+                    break;
+                }
+               
+            }
+            
+        }
+        private void Loop_Bots()
+        {
+            string text = "";
+            
+            while (true)
+            {
+                string read = Get_CQ_Call();
+                
+                if (read == "<sk>")
+                {
+                    text = "bye <sk>";
+                    string text_tx = text.Replace("<sk>", "@");
+                    string[] tx = text_tx.Split(' ');
+                    foreach (var w in tx)
+                    {
+                        data = mopp.Sent_Mopp_Word(w);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                    AppendTextBox("Server: " + text + "\r\n");
+                    Loop_Start();
+                    break;
+                }
+            }
+           
+            
+        }
+
+
+        private void Loop_Repeat()
+        {
+            string text_r = "";
+            while (true)
+            {
+                data = new byte[1024];
+                string read = "";
+                recv = newsock.ReceiveFrom(data, ref Remote);
+
+                read = mopp.Read_Mopp_Word(data);
+
+                string read_s = read.Replace("<kn>", "$");
+                read_s = read_s.Replace("<sk>", "@");
+                if (read == "*")
+                {
+                    text_r = text_r.Trim();
+                    for (var a = text_r.Length - 1; a > 0; a--)
+                    {
+                        if (text_r[a] != ' ')
+                        {
+                            text_r = text_r.Remove(a);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    text_r += read + " ";
+                }
+               
+
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                if ((read == "<kn>") || (read == "qrl?") || (read == "<sk>"))
+                {
+                    AppendTextBox("Client: " + text_r + "\r\n");
+                    AppendTextBox("Server: " + text_r + "\r\n");
+                    text_r = text_r.Replace("<sk>", "@");
+                    text_r = text_r.Replace("<kn>", "$");
+                    string[] m_text = text_r.Split(' ');
+                    foreach(var s in m_text)
+                    {
+                        data = mopp.Sent_Mopp_Word(s);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+                   
+                    
+                    text_r = "";
+                    if (read == "<sk>")
+                    {
+                        string s_text = "bye <sk>";
+                        s_text = s_text.Replace("<sk>","@");
+                        string[] tx = s_text.Split(' ');
+                        foreach (var w in tx)
+                        {
+                            
+                            data = mopp.Sent_Mopp_Word(w);
+                            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                        }
+                        AppendTextBox("Server: bye <sk>\r\n");
+                        Loop_Start();  break; 
+                    }
+                   
+                      
+                      
+                   
+                    
+
+                }
+
+
+                string text = read;
+
+
+
+
+
+            }
+        }
+        private void Loop_Small_Talk()
+        {
+
+            string text_r = "";
+            while (true)
+            {
+                data = new byte[1024];
+                string read = "";
+                recv = newsock.ReceiveFrom(data, ref Remote);
+
+                read = mopp.Read_Mopp_Word(data);
+
+                string read_s = read.Replace("<kn>", "$");
+                read_s = read_s.Replace("<sk>", "@");
+                if (read == "*")
+                {
+                    text_r = text_r.Trim();
+                    for(var a = text_r.Length-1;a>0;a--)
+                    {
+                        if(text_r[a]!=' ')
+                        { 
+                            text_r = text_r.Remove(a); 
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    text_r += read + " ";
+                }
+
+
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                if ((read == "<kn>") ||  (read == "<sk>"))
+                {
+                    AppendTextBox("Client: " + text_r + "\r\n");
+                    text_r = text_r.Replace("<kn>","");
+                    string answer = bots.Bot_Answer(text_r, "Call");
+           
+                    answer += " <kn>";
+                    string[] m_text = answer.Split(' ');
+                    foreach (var s in m_text)
+                    {
+                        data = mopp.Sent_Mopp_Word(s);
+                        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                    }
+
+                    AppendTextBox("Server: " + answer +  "\r\n");
+                    text_r = "";
+                    if (read == "<sk>")
+                    {
+                        string s_text = "bye <sk>";
+                        s_text = s_text.Replace("<sk>", "@");
+                        string[] tx = s_text.Split(' ');
+                        foreach (var w in tx)
+                        {
+
+                            data = mopp.Sent_Mopp_Word(w);
+                            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+                        }
+                        AppendTextBox("Server: bye <sk>\r\n");
+                        Loop_Start(); break;
+                    }
+
+
+
+
+
+
+                }
+
+
+                string text = read;
+
+
+
+
+
+            }
+        }
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+            DateTime date = DateTime.UtcNow;
+            txtStatusText.Text = value + txtStatusText.Text;
+           
+            using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter("Log.txt", true))
+            {
+                file.WriteLine("UTC: " + date + ": " + value);
+            }
+        }
+
+       
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                run_Server.Start();
+            }
+            catch { Start_Server(); }
+        }
+
+       
+
+      
+
+        private void Server_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            newsock.Close();
+            run_Server.Abort();
+            Application.Exit();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            newsock.Close();
+            run_Server.Abort();
+            Application.Exit();
+        }
+
+        private void btnSent_Click(object sender, EventArgs e)
+        {
+
+            string sentText = "";
+            sentText = txtSentText.Text;
+            sentText = sentText.Replace("<kn>", "$");
+            sentText = sentText.Replace("<sk>", "@");
+            if (sentText != "")
+            {
+                AppendTextBox("Server: " + sentText + "\r\n");
+
+                txtSentText.Text = "";
+
+                if (sentText != "")
+                {   //This code is for testing the sent up it is Mopp correct
+                    string[] tx = sentText.Split(' ');
+                    string read_tx = "";
+                    if (Remote != null)
+                    {
+                        foreach (var t in tx)
+                        {
+                            byte[] SentBytes = mopp.Sent_Mopp_Word(t);
+                            newsock.SendTo(SentBytes, SentBytes.Length, SocketFlags.None, Remote);
+                            string read = mopp.Read_Mopp_Word(SentBytes);
+                            read_tx += read + " ";
+                        }
+                        AppendTextBox(read_tx + "\r\n");
+                    }
+                    else
+                    {
+                        MessageBox.Show("You have to have a client connect before you can sent a text!");
+                    }
+                }
+                
+
+            }
+            else
+            {
+                MessageBox.Show("You can not sent a emty text!");
+            }
+        }
+    }
+}
